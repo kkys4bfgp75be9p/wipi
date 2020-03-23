7862259ab792b5ead10713bf2ec91d7b3988eeab
Ming Zhang <mzh@MacBook-Pro.local>
2020-03-23 16:18:37 +0100

    add config service

--- packages/client/src/hooks/useMenus.ts
+++ packages/client/src/hooks/useMenus.ts
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { PageProvider } from "@providers/page";
 
 const defaultMenus = [
